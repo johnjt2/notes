82b63438e6108f2f6586b0d9ba15d25f0149be5a
johnjt2 <144230804+johnjt2@users.noreply.github.com>
2023-09-06 13:51:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+	const btnText = document.getElementById('btn')
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+ btnText.innerText = "Copied Text"
+  
+
+}
